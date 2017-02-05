6dd7892c948aa8eba89ab824c14be554e1decca2
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-02-05 18:34:06 -0500

    Main Scene

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    LevelManager levelManager;
+    GameObject mainCamera;
+
+    private void Start()
+    {
+        levelManager = GetComponent<LevelManager>();
+        LevelManager.DontDestroyOnLoad(levelManager);
+    } 
+
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadStartScene ()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
