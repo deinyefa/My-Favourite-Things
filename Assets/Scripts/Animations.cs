27ada054a6548cf75360415c3d62ddbbccf540eb
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-02-07 09:31:20 -0500

    feat: waypoints and some animation

---
+++ Assets/Scripts/Animations.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations : MonoBehaviour {
+
+    private Animator anim;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    public void AnimateBooks ()
+    {
+        anim.SetBool("ViewingBooks", true);
+    }
+
+    public void DontAnimate ()
+    {
+        anim.SetBool("ViewingBooks", false);
+        anim.SetBool("ViewingSneakers", false);
+    }
+
+    public void AnimateSneakers ()
+    {
+        anim.SetBool("ViewingSneakers", true);
+    }
+}
