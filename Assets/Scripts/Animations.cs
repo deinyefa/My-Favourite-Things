4678c5a53a1e5e77f9c8aaa433428ac75c6e7433
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-02-07 10:18:21 -0500

    feat: some more animations

--- Assets/Scripts/Animations.cs
+++ Assets/Scripts/Animations.cs
@@ -15,14 +15,26 @@ public class Animations : MonoBehaviour {
         anim.SetBool("ViewingBooks", true);
     }
 
-    public void DontAnimate ()
-    {
-        anim.SetBool("ViewingBooks", false);
-        anim.SetBool("ViewingSneakers", false);
-    }
-
     public void AnimateSneakers ()
     {
         anim.SetBool("ViewingSneakers", true);
+    }
+
+    public void AnimateXbox ()
+    {
+        anim.SetBool("ViewingXbox", true);
+    }
+
+    public void AnimateCardboard()
+    {
+        anim.SetBool("ViewingCardboard", true);
+    }
+
+    public void DontAnimate()
+    {
+        anim.SetBool("ViewingBooks", false);
+        anim.SetBool("ViewingSneakers", false);
+        anim.SetBool("ViewingXbox", false);
+        anim.SetBool("ViewingCardboard", false);
     }
 }
